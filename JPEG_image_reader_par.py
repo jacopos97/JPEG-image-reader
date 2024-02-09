337a1c497a8aa180fa6c080d746d6ac0836fc303
jacopo <jacopo.sabatino@edu.unifi.it>
2024-02-09 20:07:47 +0100

    Added asynchronus version and changed the sequential.

--- JPEG_image_reader_par.py
+++ JPEG_image_reader_par.py
@@ -4,22 +4,31 @@ import matplotlib.pyplot as plt
 import math
 import time
 from io import BytesIO
+import asyncio
+from concurrent.futures import ThreadPoolExecutor
+import multiprocessing
 
 
 class JPEGImageReader:
     def __init__(self, directory) -> None:
         self.directory = directory
-        self.images = []
+        manager = multiprocessing.Manager()
+        self.images = manager.list()
 
-    def read_images(self, num_images=None) -> None:
+    async def read_images(self, num_images=None) -> None:
         print("Reading images...")
         image_files = os.listdir(self.directory)
         if num_images is not None:
             image_files = image_files[:num_images]
-        for filename in image_files:
-            if filename.lower().endswith('.jpg') or filename.lower().endswith('.jpeg'):
-                image_path = os.path.join(self.directory, filename)
-                self.open_image(image_path)
+        loop = asyncio.get_running_loop()
+        with ThreadPoolExecutor() as pool:
+            tasks = []
+            for filename in image_files:
+                if filename.lower().endswith('.jpg') or filename.lower().endswith('.jpeg'):
+                    image_path = os.path.join(self.directory, filename)
+                    task = loop.run_in_executor(pool, self.open_image, image_path)
+                    tasks.append(task)
+            await asyncio.gather(*tasks)
         print("Images loaded.")
 
     def open_image(self, image_path) -> None:
@@ -52,15 +61,16 @@ class JPEGImageReader:
         for image in self.images:
             image.close()
 
-if __name__ == "__main__":
+
+async def main() -> None:
     directory_path = os.path.abspath("images")
     reader = JPEGImageReader(directory_path)
     start_time = time.time()
-    reader.read_images(40000)
+    await reader.read_images(40000)
     execution_time = time.time() - start_time
     print("Time in seconds to read images: %s" % execution_time)
-    #all_images = reader.get_all_images()
-    #for filename, image in all_images.items():
-        #print(f"Filename: {filename}, Size: {image.size}")
     #reader.show_images(100)
     reader.close_images()
+
+if __name__ == "__main__":
+    asyncio.run(main())
